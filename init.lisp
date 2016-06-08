7c87fe95296ac1b03bf232db1b524b01ec840101
Kenneth W Brown III <pliny@ptelder.net>
2016-06-08 13:19:06 -0700

    Added check for tangled file to init.lisp

--- init.lisp
+++ init.lisp
@@ -7,5 +7,20 @@
 ;; it if needed. Hopefully I pick up enough CLisp along the
 ;; way to make that happen...
 
-(load "/home/pliny/.stumpwm.d/stump_conf.lisp")
+;; Well... Now it checks for the existence of the tangled file.
+;; Now we just have to tangle if missing, then do the freshness check.
+;; The func FILE-WRITE-DATE will spit out the unix time for a file
 
+(if
+ (probe-file
+  ;; When initializing, StumpWM seems to use ~ as the working directory...
+  (concatenate 'string
+	       (namestring (probe-file "."))
+	       ".stumpwm.d/"
+	       "stump_conf.lisp"))
+ (load (concatenate 'string
+		    (namestring (probe-file "."))
+		    ".stumpwm.d/"
+		    "stump_conf.lisp"))
+ (setf *startup-message* "Tangled stump_conf.lisp missing!")
+ )
